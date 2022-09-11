5ed59ee07c5041a6060a7fdddd0c9cc8a777f28a
sujithkumara <sujith.sunu19@gmail.com>
2022-09-11 07:36:55 +0000

    gitpod-stuff

---
+++ gitpod.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pip install -q -r requirements.txt
+pip -q install pyngrok==4.1.1 pymysql flask flask_ngrok flask_sqlalchemy flask_login PyPDF2==2.4.0
+ngrok authtoken '2EaH7NdHZUkj1Ulc2tOh2rluSHA_2219GaUreB6qSpiWNRs8k'
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'"
+echo -e "[mysql]\nuser = root\npassword = root" > ~/.my.cnf
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS studentdbms;"
+echo "USE studentdbms;" > query.sql
+cat students.sql >> query.sql
+mysql -u root < query.sql && rm query.sql
+python3 main.py
